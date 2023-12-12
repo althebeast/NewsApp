2a1e6173ec092befd27cd016146b396b0270ce1c
althebeast <alperensarisan@icloud.com>
2023-12-11 22:41:14 -0400

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Alperen Sarışan on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
